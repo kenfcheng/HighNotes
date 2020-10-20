de15ee518437a420765467391035ec2fabfc6de5
Dwayne Fonville Jr <64673857+dfonville20@users.noreply.github.com>
2020-10-20 18:38:06 -0400

    formatting error

--- client/src/pages/welcome.js
+++ client/src/pages/welcome.js
@@ -6,12 +6,12 @@ import map;
 class ButtonContainer extends React.Component {
     render() {
         return (
-                <div className="col-xs-12 col-sm-6 example-col">
-                    <p>Options</p>
-                    <p>
-                        <Button primary={true}>Browse</Button>
-                    </p>
-                </div>
+            <div className="col-xs-12 col-sm-6 example-col">
+                <p>Options</p>
+                <p>
+                <Button primary={true}>Browse</Button>
+                </p>
+            </div>
         );
     }
 }
