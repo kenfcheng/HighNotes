a815a55a76355a6cf978a51fcfe67f88ea59cb97
Dwayne Fonville Jr <64673857+dfonville20@users.noreply.github.com>
2020-10-20 18:58:57 -0400

    force push

--- client/src/pages/welcome.js
+++ client/src/pages/welcome.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Button } from '@progress/kendo-react-buttons';
-import map;
+import map; force push
 
 class ButtonContainer extends React.Component {
     render() {
