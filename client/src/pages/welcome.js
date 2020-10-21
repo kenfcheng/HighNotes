070cc155a6858c45ea112ba8948290e680de75a9
Dwayne Fonville Jr <64673857+dfonville20@users.noreply.github.com>
2020-10-20 22:12:55 -0400

    update buttons and map pending revisions

--- client/src/pages/welcome.js
+++ client/src/pages/welcome.js
@@ -1,21 +1,63 @@
+// import googMap from '../components/googMap';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Button } from '@progress/kendo-react-buttons';
-import map; force push
+
+let map, infoWindow;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 6,
+  });
+  infoWindow = new google.maps.InfoWindow();
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+        infoWindow.setPosition(pos);
+        infoWindow.setContent("Location found.");
+        infoWindow.open(map);
+        map.setCenter(pos);
+      },
+      () => {
+        handleLocationError(true, infoWindow, map.getCenter());
+      }
+    );
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
+}
 
 class ButtonContainer extends React.Component {
     render() {
         return (
-            <div className="col-xs-12 col-sm-6 example-col">
-                <p>Options</p>
-                <p>
-                <Button primary={true}>Browse</Button>
-                </p>
+            <div className="row example-wrapper">
+                <div className="col-xs-12 col-sm-6 example-col">
+                    <p>
+                        <Button primary={true}>Profile</Button>;
+                        <Button primary={true}>Map</Button>;
+                        <Button primary={true}>Messages</Button>;
+                    </p>
+                </div>
             </div>
-        );
+        )
     }
+
 }
-ReactDOM.render(
-    <ButtonContainer />,
-    document.querySelector('my-app')
-);
